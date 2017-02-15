cc57c39aefdf878c8e1acb51f4d28c5d1373b771
Sean Raffuse <raffscallion@gmail.com>
2017-02-15 09:52:40 -0800

    Add VIIRS Refactor ProcessTranche

---
+++ config/VIIRSi.R
@@ -0,0 +1,29 @@
+# SF Configuration file for Tranche 3 Data
+# VIIRS i-band
+
+input.name <- 'VIIRS-i'  # A friendly name for metadata and the output name
+inname <- "VIIRS_i_2015_MN.txt"
+inpath <- "./InputData/VIIRS_I/"
+outpath <- './FinalData/Tranche3'
+
+# How the SF standard fields should be calculated.  These are
+# passed to mutate()
+#ID <- "rownames(points@data)"
+ID <- "row_number()"
+AREA <- 50
+START <- "as.Date(strptime(DATE, format='%m/%d/%Y'))"
+END <- "as.Date(strptime(DATE, format='%m/%d/%Y'))"
+TYPE <- "'unknown'"
+NAME <- "'unknown'"
+SOURCE <- "'VIIRS-i'"
+
+# Point data (csv) specific parameters
+within.distance <- 1500   # Distance in meters considered associated with the primary poly (should relate to spatial uncertainty)
+columns <- c(NA, NA, NA, NA, NA, 'numeric', 'numeric', NA, NA, NA, NA, NA, NA, NA, NA)
+coord.fields <- c("LONG", "LAT")
+
+# Size (in acres) to assume for a pixel in the absence of anything else
+NOMINAL.SIZE = 50
+
+# Nominal nadir pixel resolution (in meters)
+PIXEL.RESOLUTION <- 350
